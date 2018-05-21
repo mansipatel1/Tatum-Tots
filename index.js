3b294a74ec4011998b57bb5db646c2a68bb0a4af
Mansi <mansigpatel1@gmail.com>
2018-05-21 14:43:02 -0400

    added edit button

--- index.js
+++ index.js
@@ -22,17 +22,66 @@ class App {
 
       }
 
+    favFlick(item, flick, _ev) {
+        flick.fav = item.classList.toggle('fav');
+
+    }
+    saveOnEnter(flick, ev) {
+        if (ev.key === 'Enter') {
+          this.toggleEditable(flick, ev)
+        }
+      }
+
+    toggleEditable(item, flick, _ev) {
+        const item = ev.target.closest('.flick')
+        const btn = item.querySelector('.edit.button')
+        const nameField = item.querySelector('.flickName')
+    
+        if (nameField.isContentEditable) {
+          // make it no longer editable
+          nameField.contentEditable = false
+          btn.textContent = 'edit'
+          btn.classList.remove('success')
+    
+          // save changes
+          flick.name = nameField.textContent
+        } else {
+          // make it editable
+          nameField.contentEditable = true
+          nameField.focus()
+          btn.textContent = 'save'
+          btn.classList.add('success')
+        }
+      }
+
     renderListItem(flick) {
       const item = this.template.cloneNode(true);
       item.classList.remove('template');
       item.dataset.id = flick.id;
+
+      const nameSpan = item.querySelector('.flickName')
+      nameSpan.textContent = flick.name
+      nameSpan.addEventListener(
+        'keypress',
+        this.saveOnEnter.bind(this, flick)
+      )
+
       item
         .querySelector('.flickName')
         .textContent = flick.name;
 
         item
         .querySelector('.remove.button')
-        .addEventListener('click',this.removeFlick.bind(this, flick))
+        .addEventListener('click',this.removeFlick.bind(this, item, flick))
+
+        item
+        .querySelector('.fav.button')
+        .addEventListener('click',this.favFlick.bind(this, item, flick))
+
+
+        item
+        .querySelector('.edit.button')
+        .addEventListener('click',this.toggleEditable.bind(this, flick))
 
       return item
     }
@@ -43,6 +92,7 @@ class App {
         const flick = {
             id: ++this.max,
             name: f.flickName.value,
+            fav: false
         }
     
         this.flicks.unshift(flick)
@@ -54,9 +104,7 @@ class App {
         }
   }
   
-  const app = new App();
-
-  app.init({
+  const app = new App({
     formSelector: '#flickForm',
     listSelector: '#flickList',
     templateSelector: '.flick.template',
